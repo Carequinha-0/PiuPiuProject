a7b1630664995c4afe3c0c052d3552d152d0d2e5
Liberontissauri <vaz.liberato@gmail.com>
2023-11-29 13:51:45 +0000

    Implements player movement and adds idle animation

---
+++ Components/Entities/Player/States/walking.cs
@@ -0,0 +1,55 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class PlayerWalkingState : State
+{
+    public new player parent_node;
+    public PlayerWalkingState(player parent_node) : base(parent_node)
+    {
+        this.parent_node = parent_node;
+    }
+    public float x_strength;
+    public float y_strength;
+
+    override public void _EnterState() {
+    }
+
+    override public void _ExitState() {
+
+    }
+    override public void _InputProcess() {
+        x_strength = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+        y_strength = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
+    }
+    
+
+    override public State _process_state(double delta) {
+        Vector2 speed_vector = new Vector2
+        {
+            X = x_strength,
+            Y = y_strength
+        };
+        if(speed_vector == Vector2.Zero){
+            return new PlayerIdleState(parent_node);}
+        speed_vector = speed_vector.Normalized();
+        parent_node.player_movement_velocity = speed_vector * parent_node.SPEED;
+
+        AnimationPlayer animationPlayer = this.parent_node.GetNode<AnimationPlayer>("./AnimationPlayer");
+        animationPlayer.CurrentAnimation = GetAnimationNameBySpeed(speed_vector);
+
+        return null;
+    }
+
+    override public State _physics_process_state(double delta) {
+        return null;
+    }
+
+    public string GetAnimationNameBySpeed(Vector2 speed_vector) {
+        if(speed_vector.X > 0) return "running_right";
+        if(speed_vector.X < 0) return "running_left";
+        if(speed_vector.Y < 0) return "running_up";
+        if(speed_vector.Y > 0) return "running_down";
+        return null;
+    }
+}
--- Components/Entities/Player/player.cs
+++ Components/Entities/Player/player.cs
@@ -1,24 +1,113 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 public partial class player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public float SPEED = 200f;
+	public Vector2 player_movement_velocity = Vector2.Zero;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	public enum PlayerStates {
+		Idle,
+		WalkingUp,
+		WalkingDown,
+		WalkingLeft,
+		WalkingRight,
+	}
+
+	public PlayerStates state = PlayerStates.Idle;
+
+	public StateMachine stateMachine;
+    public override void _Ready()
+    {
+		var defaultstate = new PlayerIdleState(this);
+		stateMachine = new StateMachine(defaultstate);
+        base._Ready();
+    }
+    public override void _Process(double delta)
+	{
+		stateMachine.Input_Process();
+		stateMachine.Process(delta);
+		//player_movement_velocity = GetMovementVector();
+		//if (player_movement_velocity == Vector2.Zero) EnterState(PlayerStates.Idle);
+		//else HandleWalkingStates(player_movement_velocity);
+
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 new_velocity = Velocity;
+		
+		stateMachine.Physics_Process(delta);
+		Vector2 final_velocity = Vector2.Zero;
+		final_velocity += player_movement_velocity;
 
-		if(Input.IsKeyPressed(Key.W)) new_velocity = new Vector2(0,-40);
-		if(Input.IsKeyPressed(Key.S)) new_velocity = new Vector2(0, 40);
-		if(Input.IsKeyPressed(Key.D)) new_velocity = new Vector2(40, 0);
-		if(Input.IsKeyPressed(Key.A)) new_velocity = new Vector2(-40, 0);
+		Velocity = final_velocity;
 
-		Velocity = new_velocity;
 		MoveAndSlide();
+	}
+
+
+
+	public void HandleWalkingStates (Vector2 player_movement_velocity) {
+		const float UP_ANGLE = (float) Math.PI/2;
+		const float LEFT_ANGLE = (float) Math.PI;
+		const float DOWN_ANGLE = (float) (Math.PI + Math.PI/2);
+		const float RIGHT_ANGLE = (float) Math.PI * 2;
+
+		float movement_velocity_angle = player_movement_velocity.Angle();
+		GD.Print(movement_velocity_angle);
+		if (movement_velocity_angle < (float) UP_ANGLE) EnterState(PlayerStates.WalkingRight);
+		else if (movement_velocity_angle < (float) LEFT_ANGLE) EnterState(PlayerStates.WalkingUp);
+		else if (movement_velocity_angle < (float) DOWN_ANGLE) EnterState(PlayerStates.WalkingLeft);
+		else if (movement_velocity_angle < (float) RIGHT_ANGLE) EnterState(PlayerStates.WalkingDown);
+	}
+
+	public void EnterState(PlayerStates new_state) {
+		PlayerStates previous_state = this.state;
+		this.state = new_state;
+
+		switch (this.state)
+		{
+			case PlayerStates.Idle:
+				EnterIdle();
+				break;
+			case PlayerStates.WalkingUp:
+				EnterWalkingUp();
+				break;
+			case PlayerStates.WalkingDown:
+				EnterWalkingDown();
+				break;
+			case PlayerStates.WalkingLeft:
+				EnterWalkingLeft();
+				break;
+			case PlayerStates.WalkingRight:
+				EnterWalkingRight();
+				break;
+		}
+	}
+
+	public void EnterIdle() {
+		AnimationPlayer animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		animationPlayer.Play("idle");
+	}
+	public void EnterWalkingUp() {
+		AnimationPlayer animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		animationPlayer.Play("idle");
+		//GD.Print("UP");
+	}
+	public void EnterWalkingDown() {
+		AnimationPlayer animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		animationPlayer.Play("idle");
+		//GD.Print("DOWN");
+	}
+	public void EnterWalkingLeft() {
+		AnimationPlayer animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		animationPlayer.Play("idle");
+		//GD.Print("LEFT");
+	}
+	public void EnterWalkingRight() {
+		AnimationPlayer animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		animationPlayer.Play("idle");
+		//GD.Print("RIGHT");
 	}
 }
