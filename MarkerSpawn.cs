7547c72d71fb8260a07ed80ae0a20298c878c253
Lactyos <dmfav06@gmail.com>
2024-01-09 17:13:56 +0000

    Sync

--- MarkerSpawn.cs
+++ MarkerSpawn.cs
@@ -1,7 +1,7 @@
-/*using Godot;
+using Godot;
 using System;
 
-public partial class Main : Node2D
+public partial class MarkerSpawn : Marker2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -11,12 +11,14 @@ public partial class Main : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+        
 	}
 	
 	public void OnTimerTimeout()
 	{
-		var scene = GD.Load<PackedScene>("res://CloseRangeMob.tscn");
+        var scene = GD.Load<PackedScene>("res://CloseRangeMob.tscn");
 		var enemy1 = scene.Instantiate<CharacterBody2D>();
 		AddChild(enemy1);
+        enemy1.Position = this.Position;
 	}
-} */
+}
