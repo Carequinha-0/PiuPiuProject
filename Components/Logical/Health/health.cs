374333aa7e3b1eab7e9a5448a66a9f0f959673cf
Rodri321 <151630168+Rodri321@users.noreply.github.com>
2024-01-15 16:20:05 +0000

    3 levels

--- Components/Logical/Health/health.cs
+++ Components/Logical/Health/health.cs
@@ -4,32 +4,32 @@ using Godot;
 
 public class Health
 {
-    public float max_health;
-    public float current_health;
-    public Action OnDeath;
+	public float max_health;
+	public float current_health;
+	public Action OnDeath;
 
-    public Health(float max_health, Action onDeath) {
-        this.max_health = max_health;
-        this.current_health = max_health;
-        this.OnDeath = onDeath;
-    }
+	public Health(float max_health, Action onDeath) {
+		this.max_health = max_health;
+		this.current_health = max_health;
+		this.OnDeath = onDeath;
+	}
 
-    public void TakeDamage(float damage) {
-        current_health -= damage;
-        if (current_health <= 0) {
-            OnDeath();
-        }
-    }
-    public void Heal(float heal_amount) {
-        current_health += heal_amount;
-        if (current_health > max_health) {
-            current_health = max_health;
-        }
-    }
-    public void HealFull() {
-        this.current_health = max_health;
-    }
-    public float GetHealthPercentage() {
-        return (current_health / max_health) * 100;
-    }
+	public void TakeDamage(float damage) {
+		current_health -= damage;
+		if (current_health <= 0) {
+			OnDeath();
+		}
+	}
+	public void Heal(float heal_amount) {
+		current_health += heal_amount;
+		if (current_health > max_health) {
+			current_health = max_health;
+		}
+	}
+	public void HealFull() {
+		this.current_health = max_health;
+	}
+	public float GetHealthPercentage() {
+		return (current_health / max_health) * 100;
+	}
 }
