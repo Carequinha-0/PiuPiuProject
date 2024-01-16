b632a4fb6f1521b7df7baad095d50ab7dea76261
Rodri321 <151630168+Rodri321@users.noreply.github.com>
2024-01-16 00:05:29 +0000

    toma

--- Components/Main menu/MainMenu.cs
+++ Components/Main menu/MainMenu.cs
@@ -7,7 +7,10 @@ public partial class MainMenu : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		audioStreamPlayer = GetNode<AudioStreamPlayer>("./AudioStreamPlayer");
 	}
+
+	public AudioStreamPlayer audioStreamPlayer;
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
